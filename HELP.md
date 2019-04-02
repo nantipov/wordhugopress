7ea56efafadbda79717dd7a436e8feb4b409be50
Nikolay Antipov <nantipov@users.noreply.github.com>
2019-04-02 14:12:54 +0200

    Initialize project

---
+++ HELP.md
@@ -0,0 +1,8 @@
+# Getting Started
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Creating a Batch Service](https://spring.io/guides/gs/batch-processing/)
+* [Accessing data with MySQL](https://spring.io/guides/gs/accessing-data-mysql/)
+
---
+++ src/main/java/org/nantipov/utils/wordpresstohugo/WordpresstohugoApplication.java
@@ -0,0 +1,13 @@
+package org.nantipov.utils.wordpresstohugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordpresstohugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordpresstohugoApplication.class, args);
+	}
+
+}
